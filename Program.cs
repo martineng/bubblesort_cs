fc77cba2586c02be90417b327d46baadcd0d7baa
martineng <martin.eca.6651@gmail.com>
2019-04-08 16:02:05 +0800

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+namespace bubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Generate Random Number
+            Random random = new Random();
+            int randLength = random.Next(4,12); // Random 4 - 12
+
+            int [] numArray = new int[randLength];
+
+            // Assign Value
+            for (int count = 0; count!=randLength; count++)
+            {
+                numArray[count] = random.Next(1, 100); // Random 1 -100
+            }
+
+            // Print
+            Console.WriteLine("Array: ");
+            for (int count = 0; count!= randLength; count++)
+            {
+                Console.WriteLine(numArray[count]);
+            }
+
+            Console.WriteLine("==========Bubble Sort=====================");
+             
+            // Initialize and Copy Array
+            int [] sortedArray = new int[randLength];
+            sortedArray = CopyArray(numArray, randLength);
+            sortedArray = BubbleSort(sortedArray, randLength);
+
+            // Print
+            Console.WriteLine("\nSorted Array: ");
+            for (int count = 0; count!=randLength; count++)
+            {
+                Console.WriteLine(sortedArray[count]);
+            }
+
+            Console.WriteLine("==========Optimized Bubble Sort===============");
+            
+            // Initialize and Copy Array
+            sortedArray = new int[randLength];
+            sortedArray = CopyArray(numArray, randLength);
+            sortedArray = OptimizedBubbleSort(sortedArray, randLength);
+
+            // Print
+            Console.WriteLine("\nSorted Array: ");
+            for (int count = 0; count != randLength; count++)
+            {
+                Console.WriteLine(sortedArray[count]);
+            }
+        } // END Main()
+
+        private static int[] BubbleSort(int[] inNumArray, int inLength)
+        {
+            for (int i = 0;i < inLength-1;i++) // Scan the Array
+            {
+                Console.WriteLine("Scan " + (i+1) + ":");
+                for (int j = 0;j < inLength-i-1; j++) // Compare Value
+                {
+                    // IF [j] > [j+1], Swap Position
+                    if (inNumArray[j] > inNumArray[j+1])
+                    {
+                        Console.WriteLine("......inNumArray["+j+"]:" + inNumArray[j] + " > inNumArray[" + (j+1) + "]:" + inNumArray[j+1]);
+                        int temp = inNumArray[j];
+                        inNumArray[j] = inNumArray[j+1];
+                        inNumArray[j+1] = temp;
+                    } // END IF
+                } // END FOR j
+            } // END FOR i
+
+            return inNumArray;
+
+        } // END BubbleSort()
+
+        private static int[] OptimizedBubbleSort(int[] inNumArray, int inLength)
+        {
+            bool swap = false;
+
+            for (int i = 0;i < inLength-1;i++)
+            {
+                swap = false;
+                Console.WriteLine("Scan " + (i+1) + ":");
+
+                for (int j = 0; j < inLength - i - 1; j++)
+                {
+                    if (inNumArray[j] > inNumArray[j+1])
+                    {
+                        Console.WriteLine("......inNumArray["+j+"]:" + inNumArray[j] + " > inNumArray[" + (j+1) + "]:" + inNumArray[j+1]);
+                        int temp = inNumArray[j];
+                        inNumArray[j] = inNumArray[j+1];
+                        inNumArray[j+1] = temp;
+                        swap = true;
+                    } // END IF
+                } // END FOR j
+
+                // IF the inner loop no longer get any swap, stop the loop.
+                if (swap == false)
+                {
+                    return inNumArray;
+                } 
+            } // END for i
+     
+            return inNumArray;
+        } // END OptimizedBubbleSort()
+
+        private static int[] CopyArray(int[] inNumArray, int inLength)
+        {
+            int[] tempArray = new int[inLength];
+
+            for (int count = 0; count!=inLength; count++)
+            {
+                tempArray[count] = inNumArray[count]; 
+            } // END FOR
+            return tempArray;
+        } // END CopyArray()
+    }
+}
---
+++ README.md
@@ -0,0 +1,42 @@
+# This is a simple demonstration of Bubble Sort in C#
+
+Bubble Sort is the simplest Sorting Algorithm that repeatedly compare 2 elements and swapping the adjacent elements when there are not in the correct order.
+
+For Example:
+Given an array of: [5, 9, 7, 4]
+And a condition of swapping element when [i] > [i+1]
+
+First Scan: 
+[**5**, **9**, 7, 4]    5 > 9 = False; no changes.
+[5, **9**, **7**, 4]    9 > 7 = True; swap.
+[5, 7, **9**, **4**]    9 > 4 = True; swap.
+[5, 7, 4, 9]
+
+Second Scan:
+[**5**, **7**, 4, 9]    5 > 7 = False; no changes.
+[5, **7**, **4**, 9]    7 > 4 = True; swap
+[5, 4, **7**, **9**]    7 > 9 = False; no changes.
+[5, 4, 7, 9]
+
+Third Scan:
+[**5**, **4**, 7, 9]    5 > 4 = True; swap
+[4, **5**, **7**, 9]    5 > 7 = False; no changes;
+[4, 5, **7**, **9**]    7 > 9 = False; no changes;
+[4, 5, 7, 9]
+
+The values are now sorted in order, but Bubble Sort require one additional scan without any swap to conclude the data are sorted.
+
+Fourt Scan: No Swap
+[**4**, **5**, 7, 9]
+[4, **5**, **7**, 9]
+[4, 5, **7**, **9**]
+[4, 5, 7, 9]
+
+This example demonstrate that the Bubble Sort will always runs at O(n^2) even if it is a sorted data.
+
+| Worst and Average Case Time Complexity | O(n*n) | Worst case occurs when array is reverse sorted|
+| Best Case Time Complexity | O(n) | Best case occurs when array is already sorted |
+| Auxiliary Space | O(1) |  |
+| Boundary Cases | Takes minimum time (Order of n) when elements are already sorted |  |
+| Sorting in Place | Yes |  |
+| Stable | Yes |  |
